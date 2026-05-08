c9d6bb6ba94c6016111976df763b03e03e0f8302
christiansetti-pixel <christian_setti@estudante.sesisenai.org.br>
2026-05-08 18:58:44 +0000

    update

---
+++ atvidade 1/atividades/atividade39.py
@@ -0,0 +1,9 @@
+salario=1200
+c1= 200
+c2= 120
+
+total_contas = (c1 * 1.02) + (c2 * 1.02)  
+restante = salario - total_contas
+
+print("Total das contas:", total_contas)
+print("Restara do salario do joão: ", restante)
